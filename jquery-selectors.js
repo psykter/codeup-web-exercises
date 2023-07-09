1ed86806130041a1d5114da32eecd3b0b002e7c3
Rick Diaz <richard.david.diaz@gmail.com>
2023-07-09 15:43:16 -0500

    practiced click, dblcllick and hover methods.

--- jquery-selectors.js
+++ jquery-selectors.js
@@ -3,84 +3,31 @@ $(function() {
 
     $(".title").css("color", "purple");
 
-// $("*")
-    //     .css("outline", "1px solid lightblue")
-    //     .css("font-weight", "bold");
+    $('#codeup').hover(
+        function() {
+            $(this).css('background-color', '#FF0');
+        },
+        function() {
+            $(this).css('background-color', '#FFF');
+        }
+    );
 
     $("body").css("background-color", "lightgrey");
 
-    $("li").css("color", "red");
+    $("li").hover(
+        function() {
+            $(this).css("color", "red");
+        },
+        function() {
+            $(this).css("color", "black");
+        }
+    );
 
+    $("h1").click(function() {
+        $(this).css("background-color", "lightblue");
+    });
+
+    $("p").dblclick(function() {
+        $(this).css("font-size", "18px");
+    });
 });
-
-
-
-
-   // alert("The DOM is ready!");
-
-    // Use jquery to select the element with the id.
-    // Alert the contents.
-    // alert($('#links-list').html());
-
-    //const aboutContainer = $('#aboutContainer').html();
-    // alert(aboutContainerHTML);
-
-    // $('.codeup').css('border', '1px dotted red');
-
-    //JS Equivalent
-    // const codeUp = document.querySelectorAll('.codeup');
-
-    // This is obsolete and confusing
-    // Dont do this
-    // const codeUpTest = document.getElementsByClassName('codeup');
-    // });
-
-    // $('Li').css('font-size', '20px');
-    //
-    // $('h1, p, li').css('background-color', 'yellow');
-    // $('p').css('background-color', 'yellow');
-    // $('h1, p, li').css('background-color', 'yellow');
-
-    // alert($('h1').html());
-
-    // document.querySeletorAll('Li, p').forEach((element) => {
-    // const codeUp = document.getElementsByClassName('codeup');
-
-
-    // codeUp.forEach(aSingleCodeUp => {
-    //     aSingleCodeUp.style.border = '1px dotted red';
-    // });
-
-
-
-    // let $container = $("#id-container");
-    // $("#id-container").css("background-color", "red");
-
-    // Same thing but with vanilla JS
-    // let idContainer = document.querySelector("#id-container");
-    // idContainer.style.backgroundColor = "red";
-
-    // Select everything with class of "title" class and change the
-    // color to purple
-    // $(".title").css("color", "purple");
-
-
-    // same thing with JS
-    // let titleElements = document.querySelectorAll(".title");
-    // titleElements.forEach((element) => {
-    //     titleElements.style.color = "blue";
-
-
-
-        // Use the jQuery to select all 'li' elements.
-        // $('li').css('color', 'pink');
-
-        // Use the jQuery to select multiple selectors.
-        // $('h1,p, .highlight').css('font-weight', 'bold');
-
-        // Select everything.
-        // $('*')
-        //     .css('outline', '1px solid lightblue')
-        //     .css('font-weight', 'bold');
-
-// $('[data-id]').val();
