0ff0a3b11e1949d1f0c5b0eabc0bec972b938673
Rick Diaz <richard.david.diaz@gmail.com>
2023-07-10 23:27:53 -0500

    astroid game added starship

--- astroids.js
+++ astroids.js
@@ -1,4 +1,11 @@
 let asteroids = [];
+let starshipImage;
+let soundtrack;
+
+function preload() {
+    starshipImage = loadImage('starship.png');
+    soundtrack = new Audio('a-space-journey-through-the-solar-system-153272.mp3');
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -17,6 +24,12 @@ function draw() {
         asteroid.update();
         asteroid.display();
     }
+
+    // Calculate the reduced size of the starship image
+    let reducedSize = starshipImage.width / 15;
+
+    // Display the starship image at the center of the canvas with reduced size
+    image(starshipImage, (width - reducedSize) / 2, (height - reducedSize) / 2, reducedSize, reducedSize);
 }
 
 class Asteroid {
@@ -43,3 +56,33 @@ class Asteroid {
         ellipse(this.x, this.y, this.size);
     }
 }
+
+const konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+let konamiIndex = 0;
+
+$(document).keyup(function(event) {
+    if (event.keyCode === konamiCode[konamiIndex]) {
+        konamiIndex++;
+        if (konamiIndex === konamiCode.length) {
+            konamiSuccess();
+            konamiIndex = 0;
+        }
+    } else {
+        konamiIndex = 0;
+    }
+});
+
+function konamiSuccess() {
+    // Display the updated alert message
+    alert('You have added 30 lives!');
+}
+
+// Pause the soundtrack when the window loses focus
+$(window).blur(function() {
+    soundtrack.pause();
+});
+
+// Resume the soundtrack when the window regains focus
+$(window).focus(function() {
+    soundtrack.play();
+});
