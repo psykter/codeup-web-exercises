846d477e4d85b24b609a3860abaf5eb86cfb6ad8
Rick Diaz <richard.david.diaz@gmail.com>
2023-12-05 02:01:15 -0600

    organized and cleaned up code

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,12 @@
-//      >>>>>>>>>>>>>>>>>>>>  IFFE Wrapper  <<<<<<<<<<<<<<<<<<<<    //
+//      >>>>>>>>>>>>>>>>>>>>>>>>  IFFE Wrapper  <<<<<<<<<<<<<<<<<<<<<<<<    //
 document.addEventListener('DOMContentLoaded', function() {
     (function () {
         "use strict";
 
-//      >>>>>>>>>>>>>>>>>>>>  Base URL for forecast API  <<<<<<<<<<<<<<<<<<<<    //
+//      >>>>>>>>>>>>>>>>>>  Base URL for forecast API  <<<<<<<<<<<<<<<<<    //
         const OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 
-//      >>>>>>>>>>>>>>>>>>>>  Create URL for request based on coordinates  <<<<<<<<<<<<<<<<<<<<    //
+//      >>>>>>>>>  Create URL for request based on coordinates  <<<<<<<<    //
         function getWeatherURL(lat, lon) {
             return `${OPEN_WEATHER_URL}?lat=${lat}&lon=${lon}&units=imperial&appid=${OPEN_WEATHER_APPID}&units=imperial`;
         }
@@ -24,67 +24,55 @@ document.addEventListener('DOMContentLoaded', function() {
         let forecastLocationName = document.querySelector('#forecastName');
         forecastLocationName.innerText = `San Antonio, Texas`
 
-// Inside your IIFE, add the following code
-
-// Function to handle city search autocomplete
+//      >>>>>>>>>  Function to handle city search autocomplete  <<<<<<<<    //
         function handleAutocomplete() {
             const citySearchInput = document.getElementById('input-find');
             const suggestionsDropdown = document.getElementById('autocomplete-suggestions');
-
-            // Event listener for input changes
+//      >>>>>>>>>>>>>>>  Event listener for input changes  <<<<<<<<<<<<<    //
             citySearchInput.addEventListener('input', () => {
                 const searchText = citySearchInput.value.trim();
-
-                // Make a request to Mapbox Geocoding API for autocomplete suggestions
-                fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?access_token=${MAPBOX_TOKEN}`)
+//      >>>>>>>  Mapbox Geocoding API request for autocomplete   <<<<<<<    //
+//      >>>>>>>>>>>>>>>  suggestions in country=US,CA,MX  <<<<<<<<<<<<<<    //
+                fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?access_token=${MAPBOX_TOKEN}&country=US,CA,MX`)
                     .then(response => response.json())
                     .then(data => {
-                        // Clear previous suggestions
+//      >>>>>>>>>>>>>>>>>>  Clear previous suggestions  <<<<<<<<<<<<<<<<    //
                         suggestionsDropdown.innerHTML = '';
-
-                        // Update suggestions dropdown or create a new one
+//      >>>>>>>  Update suggestions dropdown / create a new one  <<<<<<<    //
                         for (let i = 0; i < Math.min(5, data.features.length); i++) {
                             const suggestion = document.createElement('div');
                             suggestion.className = 'autocomplete-suggestion';
                             suggestion.textContent = data.features[i].place_name;
-
-                            // Event listener for suggestion click
+//      >>>>>>>>>>>>  Event listener for suggestion click  <<<<<<<<<<<<    //
                             suggestion.addEventListener('click', () => {
                                 citySearchInput.value = data.features[i].place_name;
-                                suggestionsDropdown.innerHTML = ''; // Clear suggestions
-                                // You can also update the weather based on the selected suggestion here
+                                suggestionsDropdown.innerHTML = '';
+//      >>>>>>>>>>  You can also update the weather based on  <<<<<<<<<    //
+//      >>>>>>>>>>>>>>>>  the selected suggestion here  <<<<<<<<<<<<<<<    //
                             });
-
                             suggestionsDropdown.appendChild(suggestion);
                         }
                     })
                     .catch(error => console.error(error));
             });
         }
-
-// Call the autocomplete function
+//      >>>>>>>>>>>>>>>  Call the autocomplete function  <<<<<<<<<<<<<    //
         handleAutocomplete();
 
-// Your existing code for Find and Home buttons
         const formFind = document.getElementById('form-find');
         const btnHome = document.getElementById('btn-home');
-
-// Event listener for form submission
+//      >>>>>>>>>>>>  Event listener for form submission  <<<<<<<<<<<<    //
         formFind.addEventListener('submit', (event) => {
             event.preventDefault();
             const searchText = document.getElementById('input-find').value.trim();
-
-            // You can add the logic to update the weather based on the entered city (searchText) here
             console.log(`Searching for city: ${searchText}`);
         });
-
-// Event listener for Home button click
+//      >>>>>>>>>>>  Event listener for Home button click  <<<<<<<<<<    //
         btnHome.addEventListener('click', () => {
             // You can add the logic to reset the weather to the default location or perform any other action
             console.log('Home button clicked');
         });
-
-
+//      >>>>>>>>>  GET function in case of a failed request  <<<<<<<<    //
         function runAjax() {
             $.ajax({
                 url: URL,
@@ -107,19 +95,17 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         runAjax();
-
+//      >>>>>>>>>>>>>  Display current date/temperature  <<<<<<<<<<<<    //
         function showDatesAndTemps(minMaxTemps) {
-            // Display current date and temperature on the first card (Day1)
+//      >>>>>>>>>>>>>  Updates HTML with requested data  <<<<<<<<<<<<    //
             Day1.lastElementChild.firstElementChild.innerText = `${minMaxTemps[0].date}:\n\n ${parseInt(minMaxTemps[0].max)} ℉`;
-
-            // Display the next 4 days on the remaining cards (Day2 to Day5)
+//      >>>>>>>>>>>>>>>>>  Display the next 4 days  <<<<<<<<<<<<<<<<<    //
             Day2.lastElementChild.firstElementChild.innerHTML = `${minMaxTemps[1].date}:<br>Min: ${parseInt(minMaxTemps[1].min)} ℉<br>Max: ${parseInt(minMaxTemps[1].max)} ℉`;
             Day3.lastElementChild.firstElementChild.innerHTML = `${minMaxTemps[2].date}:<br>Min: ${parseInt(minMaxTemps[2].min)} ℉<br>Max: ${parseInt(minMaxTemps[2].max)} ℉`;
             Day4.lastElementChild.firstElementChild.innerHTML = `${minMaxTemps[3].date}:<br>Min: ${parseInt(minMaxTemps[3].min)} ℉<br>Max: ${parseInt(minMaxTemps[3].max)} ℉`;
             Day5.lastElementChild.firstElementChild.innerHTML = `${minMaxTemps[4].date}:<br>Min: ${parseInt(minMaxTemps[4].min)} ℉<br>Max: ${parseInt(minMaxTemps[4].max)} ℉`;
         }
-
-
+//      >>>>>>>>>>  Btn Click gets weather and updates map <<<<<<<<<<    //
         function showWeather(weatherArray, icons) {
             Day1.lastElementChild.lastElementChild.innerText = weatherArray[0];
             Day1.firstElementChild.firstElementChild.src = `https://openweathermap.org/img/wn/${icons[0]}@4x.png`;
@@ -135,9 +121,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
             Day5.lastElementChild.lastElementChild.innerText = weatherArray[4];
             Day5.firstElementChild.firstElementChild.src = `https://openweathermap.org/img/wn/${icons[4]}@4x.png`;
-
         }
-
+//      >>>>>>>>  Create URL for request based on coordinates  <<<<<<<    //
         document.querySelector('button').addEventListener('click', () => {
             geocode(searchInput.value, MAPBOX_TOKEN).then((coors) => {
                 URL = getWeatherURL(coors[1], coors[0]);
@@ -148,9 +133,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 searchInput.value = '';
             })
         })
-
-
-//      >>>>>>>>>>>>>>>>>>>>  Mapbox  <<<<<<<<<<<<<<<<<<<<    //
+//      >>>>>>>>>>>>>>>>>>>>  Mapbox access token <<<<<<<<<<<<<<<<<<<<    //
         mapboxgl.accessToken = MAPBOX_TOKEN;
         const map = new mapboxgl.Map({
             container: "map",
@@ -169,8 +152,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 forecastLocationName.innerText = data;
             });
         });
-
-        // Add zoom and rotation controls to the map.
+//      >>>>>>>>>>>>>>  Adds zoom and rotation controls  <<<<<<<<<<<<    //
         map.addControl(new mapboxgl.NavigationControl());
 
            //      >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  Rain layer  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    //
@@ -196,7 +178,8 @@ document.addEventListener('DOMContentLoaded', function() {
                             maxzoom: 12
                         });
                     });
-
+//      >>>>>>>>>>  Iterates through radar frames, adjusting  <<<<<<<<    //
+//      >>>>>>>>  visibility and opacity for animation on map  <<<<<<<    //
                     let i = 0;
                     const interval = setInterval(() => {
                         if (i > apiData.radar.past.length - 1) {
@@ -233,34 +216,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 })
                 .catch(console.error);
         });
-
-        /**
-         * returnMinMaxTemps destructures the OpenWeather 5 Day / 3 Hour Forecast API response to get the daily minimum
-         *   and maximum temperatures using a list of 40 three-hour forecast blocks.
-         * @param - the data from the API response
-         * @returns - an array of objects that represent the minimum and maximum temperature of each forecast day.
-         *   Example response:
-         *   [
-         *      {
-         *          "date":"2023-07-14",
-         *          "min":97.39,
-         *          "max":106.63
-         *      },
-         *      {
-         *          "date":"2023-07-15",
-         *          "min":75.79,
-         *          "max":105.75
-         *      },
-         *      etc.
-         *   ]
-         */
+//      >>>>>>>>>  Loops through 40 three hour blocks of forecast  <<<<<<<<   //
+//      >>>>>  then creates a new temperature object for each new day  <<<<   //
         function returnMinMaxTemps({list}) {
             const minMaxTempDays = [];
-
-
-
-              //  >>>>>>>>>>>>>>>>>>>>>>>  Loops through 40 three hour blocks of forecast  <<<<<<<<<<<<<<<<<<<<<<<  //
-            //  >>>>>>>>>>>>>>>>>>>>>  then creates a new temperature object for each new day  <<<<<<<<<<<<<<<<<<<<<<  //
             list.forEach(({dt_txt, main: {temp_max, temp_min}}) => {
                 const [date, time] = dt_txt.split(' '); // splits the date time string
                 if (minMaxTempDays.length === 0 || time.startsWith('00')) { // add a new forecast day for each new date
@@ -273,35 +232,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     if (max < temp_max) currentMinMaxTemp.max = temp_max;
                 }
             });
-
             return minMaxTempDays;
         }
-
-        /*
-            EXAMPLE OF USAGE:
-
-            $.get(OPEN_WEATHER_API_URL)).done(data => {
-                    const minMaxTemps = returnMinMaxTemps(data);
-                    // use minMaxTemps data as needed
-             });
-
-         */
-
-        "use strict";
-
-        /***
-         * geocode is a method to search for coordinates based on a physical address and return
-         * @param {string} search is the address to search for the geocoded coordinates
-         * @param {string} token is your API token for MapBox
-         * @returns {Promise} a promise containing the latitude and longitude as a two element array
-         *
-         * EXAMPLE:
-         *
-         *  geocode("San Antonio", API_TOKEN_HERE).then(function(results) {
-         *      // do something with results
-         *  })
-         *
-         */
+//      >>>>>>>>  Sends request, retrives and extracts coordinates  <<<<<<    //
         function geocode(search, token) {
             let baseUrl = 'https://api.mapbox.com';
             let endPoint = '/geocoding/v5/mapbox.places/';
@@ -313,8 +246,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     return data.features[0].center;
                 });
         }
-
-
         /***
          * reverseGeocode is a method to search for a physical address based on inputted coordinates
          * @param {object} coordinates is an object with properties "lat" and "lng" for latitude and longitude
